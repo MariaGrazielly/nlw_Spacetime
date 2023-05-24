4fc87c3ff8a44d321ec27f577ec8e7a94223e405
Grazielly Martins <grazelly13@hotmail.com>
2023-05-24 17:28:48 -0300

    configurações iniciais do backend usando prisma e configeração do front usando React, Next.js, TailwindCSS e Typescript.

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+import { ReactNode } from 'react'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-baiJamjuree',
+})
+
+export const metadata = {
+  title: 'NLW SpaceTime',
+  description:
+    'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e Typescript.',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamjuree} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
